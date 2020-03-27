2e4bda9be1de62cd4f1bc0ee51d2d94d81b67abe
jaehunkim <jaehunkim7730@gmail.com>
2020-03-27 17:41:59 +0900

    Modify onRightClick params in KakaoMap

--- src/Map/component.tsx
+++ src/Map/component.tsx
@@ -36,7 +36,7 @@ export interface IKakaoMapsMapProps {
   onZoomStart?: (e: { zoomLevel: number }) => void;
   onZoomChange?: (e: { zoomLevel: number }) => void;
   onClick?: (e: { position: { lat: number, lng: number } }) => void;
-  onRightClick?: () => void;
+  onRightClick?: (e: { position: { lat: number, lng: number } }) => void;
   onDoubleClick?: (e: { position: { lat: number, lng: number } }) => void;
   onDrag?: (e: { position: { lat: number, lng: number } }) => void;
   onDragEnd?: (e: { position: { lat: number, lng: number } }) => void;
