c76f8b9e8b9e3d8799c5fd7ec2df507653b6d152
jaehunkim <kjaehun6546@gmail.com>
2020-10-18 20:42:41 +0900

    Refactor Circle Component : class >> functional

--- src/Circle/index.tsx
+++ src/Circle/index.tsx
@@ -29,179 +29,173 @@ export interface IKakaoMapsCircleProps {
   onMouseOut?: (e: { position: { lat: number, lng: number } }) => void;
 }
 
-class Circle extends React.Component<IKakaoMapsCircleProps> {
-  private circle: IKakaoCircle;
+function Circle(props: React.PropsWithChildren<IKakaoMapsCircleProps>) {
+  const { map } = React.useContext(MapContext);
 
-  context!: React.ContextType<typeof MapContext>
-  static contextType = MapContext;
-
-  public static defaultProps = {
-    radius: 10,
-    fillColor: "transparent",
-    fillOpacity: 1,
-    strokeColor: "#000",
-    strokeWeight: 1,
-    strokeOpacity: 1,
-    strokeStyle: "solid",
-    zIndex: 0,
-  };
-
-  public static propTypes = {
-    /** 맵에 표시될 좌표 */
-    center: PropTypes.shape({
-      lat: PropTypes.number.isRequired,
-      lng: PropTypes.number.isRequired,
-    }).isRequired,
-    /** 단위 : m */
-    radius: PropTypes.number,
-    /** 채움 색 */
-    fillColor: PropTypes.string,
-    /** 채움 색의 불투명도 ( 0 ~ 1 ) */
-    fillOpacity: PropTypes.number,
-    /** 채움 색 */
-    strokeColor: PropTypes.string,
-    /** 채움 불투명도 ( 0 ~ 1 ) */
-    strokeOpacity: PropTypes.number,
-    /** 선의 두께 ( 단위 : px ) */
-    strokeWeight: PropTypes.number,
-    /** 선 스타일 */
-    strokeStyle: PropTypes.oneOf(["solid", "shortdash", "shortdot", "shortdashdot", "shortdashdotdot", "dot", "dash", "dashdot", "longdash", "longdashdot", "longdashdotdot"]),
-    /** z-index 속성 값 */
-    zIndex: PropTypes.number,
-    /** (e: { position: { lat: number, lng: number } }) => void */
-    onClick: PropTypes.func,
-    /** (e: { position: { lat: number, lng: number } }) => void */
-    onMouseDown: PropTypes.func,
-    /** (e: { position: { lat: number, lng: number } }) => void */
-    onMouseMove: PropTypes.func,
-    /** (e: { position: { lat: number, lng: number } }) => void */
-    onMouseOver: PropTypes.func,
-    /** (e: { position: { lat: number, lng: number } }) => void */
-    onMouseOut: PropTypes.func,
-  };
-
-  constructor(props: IKakaoMapsCircleProps) {
-    super(props);
-
+  const circle = React.useMemo<IKakaoCircle>(() => {
     const options: IKakaoCircleOptions = {
-      center: new kakao.maps.LatLng(this.props.center.lat, this.props.center.lng),
-      radius: this.props.radius,
-      zIndex: this.props.zIndex,
-      fillColor: this.props.fillColor,
-      fillOpacity: this.props.fillOpacity,
-      strokeColor: this.props.strokeColor,
-      strokeWeight: this.props.strokeWeight,
-      strokeOpacity: this.props.strokeOpacity,
-      strokeStyle: this.props.strokeStyle,
+      center: new kakao.maps.LatLng(props.center.lat, props.center.lng),
+      radius: props.radius,
+      zIndex: props.zIndex,
+      fillColor: props.fillColor,
+      fillOpacity: props.fillOpacity,
+      strokeColor: props.strokeColor,
+      strokeWeight: props.strokeWeight,
+      strokeOpacity: props.strokeOpacity,
+      strokeStyle: props.strokeStyle,
     };
-    this.circle = new kakao.maps.Circle(options);
+    const $circle = new kakao.maps.Circle(options);
 
-    kakao.maps.event.addListener(this.circle, "click", this.onClick);
-    kakao.maps.event.addListener(this.circle, "mousemove", this.onMouseMove);
-    kakao.maps.event.addListener(this.circle, "mousedown", this.onMouseDown);
-    kakao.maps.event.addListener(this.circle, "mouseover", this.onMouseOver);
-    kakao.maps.event.addListener(this.circle, "mouseout", this.onMouseOut);
-  }
+    return $circle;
+  }, []);
 
-  public componentDidMount() {
-    this.circle.setMap(this.context.map);
-  }
+  React.useEffect(() => {
+    circle.setMap(map);
+    return () => circle.setMap(null);
+  }, []);
 
-  public componentWillUnmount() {
-    this.circle.setMap(null);
-  }
+  const listeners = React.useRef<{ [listener: string]: (...args: any[]) => void }>({});
 
-  public componentWillReceiveProps(nextProps: IKakaoMapsCircleProps) {
-    if (nextProps.center.lat !== this.props.center.lat || nextProps.center.lng !== this.props.center.lng) {
-      const latlng = new kakao.maps.LatLng(nextProps.center.lat, nextProps.center.lng);
-      this.circle.setPosition(latlng);
-    }
-
-    if (nextProps.radius !== this.props.radius) {
-      this.circle.setRadius(nextProps.radius!);
-    }
-
-    if (nextProps.fillColor !== this.props.fillColor) {
-      this.circle.setOptions({ fillColor: nextProps.fillColor });
-    }
-
-    if (nextProps.fillOpacity !== this.props.fillOpacity) {
-      this.circle.setOptions({ fillOpacity: nextProps.fillOpacity });
-    }
-
-    if (nextProps.strokeWeight !== this.props.strokeWeight) {
-      this.circle.setOptions({ strokeWeight: nextProps.strokeWeight });
-    }
-
-    if (nextProps.strokeColor !== this.props.strokeColor) {
-      this.circle.setOptions({ strokeColor: nextProps.strokeColor });
-    }
-
-    if (nextProps.strokeOpacity !== this.props.strokeOpacity) {
-      this.circle.setOptions({ strokeOpacity: nextProps.strokeOpacity });
-    }
-
-    if (nextProps.strokeStyle !== this.props.strokeStyle) {
-      this.circle.setOptions({ strokeStyle: nextProps.strokeStyle });
-    }
-
-    if (nextProps.zIndex !== this.props.zIndex) {
-      this.circle.setZIndex(nextProps.zIndex!);
-    }
-  }
-
-  private onClick = (e: IKakaoMouseEvent) => {
-    this.props.onClick?.({
+  listeners.current.onClick = function onClick(e: IKakaoMouseEvent) {
+    props.onClick?.({
       position: {
         lat: e.latLng.getLat(),
         lng: e.latLng.getLng(),
       },
-    })
+    });
   }
 
-  private onMouseMove = (e: IKakaoMouseEvent) => {
-    this.props.onMouseMove?.({
+  listeners.current.onMouseMove = function onMouseMove(e: IKakaoMouseEvent) {
+    props.onMouseMove?.({
       position: {
         lat: e.latLng.getLat(),
         lng: e.latLng.getLng(),
       },
-    })
+    });
   }
 
-  private onMouseDown = (e: IKakaoMouseEvent) => {
-    this.props.onMouseDown?.({
+  listeners.current.onMouseDown = function onMouseDown(e: IKakaoMouseEvent) {
+    props.onMouseDown?.({
       position: {
         lat: e.latLng.getLat(),
         lng: e.latLng.getLng(),
       },
-    })
+    });
   }
 
-  private onMouseOver = (e: IKakaoMouseEvent) => {
-    this.props.onMouseOver?.({
+  listeners.current.onMouseOver = function onMouseOver(e: IKakaoMouseEvent) {
+    props.onMouseOver?.({
       position: {
         lat: e.latLng.getLat(),
         lng: e.latLng.getLng(),
       },
-    })
+    });
   }
 
-  private onMouseOut = (e: IKakaoMouseEvent) => {
-    this.props.onMouseOut?.({
+  listeners.current.onMouseOut = function onMouseOut(e: IKakaoMouseEvent) {
+    props.onMouseOut?.({
       position: {
         lat: e.latLng.getLat(),
         lng: e.latLng.getLng(),
       },
-    })
+    });
   }
 
-  public render() {
-    return (
-      <CircleContext.Provider value={{ circle: this.circle }}>
-        {this.props.children}
-      </CircleContext.Provider>
-    );
-  }
+  React.useEffect(() => {
+    kakao.maps.event.addListener(circle, "click", listeners.current.onClick);
+    kakao.maps.event.addListener(circle, "mousemove", listeners.current.onMouseMove);
+    kakao.maps.event.addListener(circle, "mousedown", listeners.current.onMouseDown);
+    kakao.maps.event.addListener(circle, "mouseover", listeners.current.onMouseOver);
+    kakao.maps.event.addListener(circle, "mouseout", listeners.current.onMouseOut);
+  }, []);
+
+  React.useEffect(() => {
+    const latlng = new kakao.maps.LatLng(props.center.lat, props.center.lng);
+    circle.setPosition(latlng);
+  }, [props.center.lat, props.center.lng])
+
+  React.useEffect(() => {
+    circle.setRadius(props.radius!);
+  }, [props.radius]);
+
+  React.useEffect(() => {
+    circle.setOptions({ fillColor: props.fillColor });
+  }, [props.fillColor]);
+
+  React.useEffect(() => {
+    circle.setOptions({ fillOpacity: props.fillOpacity });
+  }, [props.fillOpacity]);
+
+  React.useEffect(() => {
+    circle.setOptions({ strokeWeight: props.strokeWeight });
+  }, [props.strokeWeight]);
+
+  React.useEffect(() => {
+    circle.setOptions({ strokeColor: props.strokeColor });
+  }, [props.strokeColor]);
+
+  React.useEffect(() => {
+    circle.setOptions({ strokeOpacity: props.strokeOpacity });
+  }, [props.strokeOpacity]);
+
+  React.useEffect(() => {
+    circle.setOptions({ strokeStyle: props.strokeStyle });
+  }, [props.strokeStyle]);
+
+  React.useEffect(() => {
+    circle.setZIndex(props.zIndex!);
+  }, [props.zIndex]);
+
+  return (
+    <CircleContext.Provider value={{ circle }}>
+      {props.children}
+    </CircleContext.Provider>
+  );
+}
+
+Circle.defaultProps = {
+  radius: 10,
+  fillColor: "transparent",
+  fillOpacity: 1,
+  strokeColor: "#000",
+  strokeWeight: 1,
+  strokeOpacity: 1,
+  strokeStyle: "solid",
+  zIndex: 0,
+};
+
+Circle.propTypes = {
+  /** 맵에 표시될 좌표 */
+  center: PropTypes.shape({
+    lat: PropTypes.number.isRequired,
+    lng: PropTypes.number.isRequired,
+  }).isRequired,
+  /** 단위 : m */
+  radius: PropTypes.number,
+  /** 채움 색 */
+  fillColor: PropTypes.string,
+  /** 채움 색의 불투명도 ( 0 ~ 1 ) */
+  fillOpacity: PropTypes.number,
+  /** 채움 색 */
+  strokeColor: PropTypes.string,
+  /** 채움 불투명도 ( 0 ~ 1 ) */
+  strokeOpacity: PropTypes.number,
+  /** 선의 두께 ( 단위 : px ) */
+  strokeWeight: PropTypes.number,
+  /** 선 스타일 */
+  strokeStyle: PropTypes.oneOf(["solid", "shortdash", "shortdot", "shortdashdot", "shortdashdotdot", "dot", "dash", "dashdot", "longdash", "longdashdot", "longdashdotdot"]),
+  /** z-index 속성 값 */
+  zIndex: PropTypes.number,
+  /** (e: { position: { lat: number, lng: number } }) => void */
+  onClick: PropTypes.func,
+  /** (e: { position: { lat: number, lng: number } }) => void */
+  onMouseDown: PropTypes.func,
+  /** (e: { position: { lat: number, lng: number } }) => void */
+  onMouseMove: PropTypes.func,
+  /** (e: { position: { lat: number, lng: number } }) => void */
+  onMouseOver: PropTypes.func,
+  /** (e: { position: { lat: number, lng: number } }) => void */
+  onMouseOut: PropTypes.func,
 };
 
 export default Circle;
